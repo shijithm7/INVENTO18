d49b49bddb86519ecbfd524207c5f19aa843f70e
sebin vincent <sebinvincent@ymail.com>
2018-02-19 12:34:00 +0530

    change button name

--- app/src/main/java/com/example/sebinvincent/invento_18/MainActivity.java
+++ app/src/main/java/com/example/sebinvincent/invento_18/MainActivity.java
@@ -8,16 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private FrameLayout fragmentcontainer;
     private Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
     }
 
