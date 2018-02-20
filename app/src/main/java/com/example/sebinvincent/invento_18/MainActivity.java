df8486e93a63bb39e1eee23bae8a3e208541103c
DELL <shijithm7@gmail.com>
2018-02-20 21:01:53 +0530

    Test by shijith_splashscreen

--- app/src/main/java/com/example/sebinvincent/invento_18/MainActivity.java
+++ app/src/main/java/com/example/sebinvincent/invento_18/MainActivity.java
@@ -1,29 +1,32 @@
 package com.example.sebinvincent.invento_18;
-
-import android.annotation.SuppressLint;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FrameLayout fragmentcontainer;
-    private Button button;
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5 * 1000);
 
+                    // After 5 seconds redirect to another intent
+                    Intent i = new Intent(getBaseContext(), Main2Activity.class);
+                    startActivity(i);
 
-
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
     }
-
-
 }
+
